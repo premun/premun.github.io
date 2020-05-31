98d8302d2264d718f415084cf5a0719d53f4fef2
Premek Vysoky <premek.vysoky@microsoft.com>
2020-05-31 10:50:43 +0200

    Add welcome page

---
+++ tipemoji/precache-manifest.106759c174970ea91620c9b39b54ed29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "736ee0dc05a6401f53872cc7f7947b1e",
+    "url": "/tipemoji/index.html"
+  },
+  {
+    "revision": "ca2b4d571cfd61f9f87e",
+    "url": "/tipemoji/static/css/main.cb5838e0.chunk.css"
+  },
+  {
+    "revision": "982079edab49bdb93472",
+    "url": "/tipemoji/static/js/2.aead1938.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tipemoji/static/js/2.aead1938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca2b4d571cfd61f9f87e",
+    "url": "/tipemoji/static/js/main.db337262.chunk.js"
+  },
+  {
+    "revision": "9c273cc3e478d80f2b38",
+    "url": "/tipemoji/static/js/runtime-main.911a1a31.js"
+  }
+]);
